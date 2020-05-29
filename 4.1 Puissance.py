daef763ad7f4e48f431405cc7ae6fea3aa30fec1
Mariam D <62890032+MansouraD@users.noreply.github.com>
2020-05-29 21:18:37 +0000

    Create 4.1 Puissance.py

---
+++ 4.1 Puissance.py
@@ -0,0 +1,9 @@
+
+def main():
+    N = int(input("Merci d'entrer un réel: "))
+    x = int(input("Merci d'entrer un entier: "))
+    p = math.pow(N,x)
+    print(N," à la puissance ",x," = ",p)
+
+if __name__ == '__main__':
+    main()
